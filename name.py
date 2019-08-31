bf567e5ab7f8d377fc1cc7353ff50d433e345a73
love306 <j127289j@gmail.com>
2019-08-31 18:28:16 +0800

    'read_input_write'

--- name.py
+++ name.py
@@ -1,21 +1,32 @@
+import os#=載入作業系統的模組
 products = []
-with open('products.csv', 'r', encoding='utf-8') as f:
-	for line in f:
-		if '商品,價格' in line:
-			continue
-		name, price = line.strip().split(',')
-		products.append([name, price])
-print(name[0])
+if os.path.isfile('products.csv'):
+	print('ya!i got some good!')
+	with open('products.csv', 'r', encoding='utf-8') as f:
+		for line in f:
+			if '名稱,價格\n' in line:
+				continue
+			name, price = line.strip().split(',')
+			products.append([name, price])
+else:
+	print('nothing in here!')
+print(products)
+#讀取檔案
+
+
 #輸入檔案
 while True:
+	
 	name = input('請輸入名稱: ')
 	if name == 'q':
 		break
 	price = input('請輸入價格: ')
-	p = []
-	p.append(name)
-	p.append(price)
-	products.append(p)
+	price = int(price)
+	#p = []
+	#p.append(name)
+	#p.append(price)
+	products.append([name, str(price)])
+	
 #列出所有購買紀錄	
 for prot in products:
 	print('產品名稱:', prot[0], '價格:', prot[1])
@@ -29,8 +40,8 @@ while True:
 	
 	product.append(pp)
 print(product)
-for p in product:
-	print(p) 
+for op in product:
+	print(op) 
 shop = []
 while True:
 	na = input('名稱:')
@@ -43,7 +54,8 @@ while True:
 
 for s in shop:
 	print(s[0],'價格:', s[1], '元')
-with open('products.csv', 'w',encoding = 'utf-8') as f:
-	#f.write('名稱,價格\n')
-	for p in products:
-		f.write(p[0] + ',' + p[1] + '\n')
+#寫入檔案
+with open('products.csv', 'w',encoding = 'utf-8') as d:	
+	d.write('名稱,價格\n')
+	for p in products: 	
+		d.write(p[0] + ',' + str(p[1]) + '\n')
