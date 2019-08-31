00e87ed80645ff2bb3ee8d1622ad7082b7a0ebbd
love306 <j127289j@gmail.com>
2019-08-31 14:26:35 +0800

    'writeandread'

--- name.py
+++ name.py
@@ -1,5 +1,12 @@
 products = []
-
+with open('products.csv', 'r', encoding='utf-8') as f:
+	for line in f:
+		if '商品,價格' in line:
+			continue
+		name, price = line.strip().split(',')
+		products.append([name, price])
+print(name[0])
+#輸入檔案
 while True:
 	name = input('請輸入名稱: ')
 	if name == 'q':
@@ -9,6 +16,7 @@ while True:
 	p.append(name)
 	p.append(price)
 	products.append(p)
+#列出所有購買紀錄	
 for prot in products:
 	print('產品名稱:', prot[0], '價格:', prot[1])
 product = []
@@ -35,6 +43,7 @@ while True:
 
 for s in shop:
 	print(s[0],'價格:', s[1], '元')
-with open('products.txt', 'w') as f:
+with open('products.csv', 'w',encoding = 'utf-8') as f:
+	#f.write('名稱,價格\n')
 	for p in products:
 		f.write(p[0] + ',' + p[1] + '\n')
