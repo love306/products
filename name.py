8b9ff510664172ac82e0b5c7179b55454b7343a3
love306 <j127289j@gmail.com>
2019-09-03 14:47:55 +0800

    'add_funtion'

--- name.py
+++ name.py
@@ -1,61 +1,49 @@
 import os#=載入作業系統的模組
-products = []
-if os.path.isfile('products.csv'):
-	print('ya!i got some good!')
-	with open('products.csv', 'r', encoding='utf-8') as f:
+#讀取檔案
+def read_file(filename):
+	products = []			
+	with open(filename, 'r', encoding='utf-8') as f:
 		for line in f:
-			if '名稱,價格\n' in line:
+			if '商品,價格\n' in line:
 				continue
 			name, price = line.strip().split(',')
-			products.append([name, price])
-else:
-	print('nothing in here!')
-print(products)
-#讀取檔案
-
-
+			products.append([name, price])		
+	return products
 #輸入檔案
-while True:
-	
-	name = input('請輸入名稱: ')
-	if name == 'q':
-		break
-	price = input('請輸入價格: ')
-	price = int(price)
-	#p = []
-	#p.append(name)
-	#p.append(price)
-	products.append([name, str(price)])
-	
+def user_input(products):	
+	while True:		
+		name = input('請輸入名稱: ')
+		if name == 'q':
+			break
+		price = input('請輸入價格: ')
+		price = int(price)
+		products.append([name, price])
+	print(products)
+	return products
 #列出所有購買紀錄	
-for prot in products:
-	print('產品名稱:', prot[0], '價格:', prot[1])
-product = []
-while True:
-	nam = input('請輸入名稱')
-	if nam == 'q':
-		break	
-	pri = input('價格')
-	pp = [nam, pri]
-	
-	product.append(pp)
-print(product)
-for op in product:
-	print(op) 
-shop = []
-while True:
-	na = input('名稱:')
-	if na == 'q':
-		break
-	pr = input('價格:')
-	shop.append([na, pr])
-#print(shop[0])
-#print(shop[1])
-
-for s in shop:
-	print(s[0],'價格:', s[1], '元')
+def print_products(products):
+	for prot in products:
+		print('產品名稱:', prot[0], '價格:', prot[1])
 #寫入檔案
-with open('products.csv', 'w',encoding = 'utf-8') as d:	
-	d.write('名稱,價格\n')
-	for p in products: 	
-		d.write(p[0] + ',' + str(p[1]) + '\n')
+def write_file(filename, products):		
+	with open(filename, 'w',encoding = 'utf-8') as d:	
+		d.write('商品,價格\n')
+		for p in products: 	
+			d.write(p[0] + ',' + str(p[1]) + '\n')
+def when_nofile(filename):
+	with open(filename, 'w',encoding = 'utf-8') as d:	
+		d.write('商品,價格\n')
+	
+def main():
+	filename = 'products.csv'  	 
+	if os.path.isfile(filename):#檢查檔案
+		print('ya!i got some good!')
+		products = read_file(filename)
+	else:
+		print('nothing in here!')
+		when_nofile(filename)
+		products = read_file(filename)				
+	user_input(products)
+	print_products(products)
+	write_file(filename, products)
+main()
